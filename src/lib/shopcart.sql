1d9189d3bdc748a0677158636aded88dae92bcda
MaxinFN <maxindeadpool@gemail.com>
2019-06-16 16:09:18 +0800

    更新数据库

--- src/lib/shopcart.sql
+++ src/lib/shopcart.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50714
 File Encoding         : 65001
 
-Date: 2019-06-15 21:33:36
+Date: 2019-06-16 16:08:46
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -36,21 +36,4 @@ INSERT INTO `shopcart` VALUES ('999', '123', '123', '123', '999', '123', '123');
 INSERT INTO `shopcart` VALUES ('123', '123', '123', '123', '123', '123', '111');
 INSERT INTO `shopcart` VALUES ('333', '123', '123', '123', '123', '123', '11');
 INSERT INTO `shopcart` VALUES ('444', '123', '123', '123', '123', '123', '101');
-INSERT INTO `shopcart` VALUES ('8', 'new3-1.jpg', 'T恤周南西毒白色衬衫', '中号', '60', '1', '1');
-INSERT INTO `shopcart` VALUES ('3', 'shirt003.jpg', '恤 山鸟叔 广告位 白色衬衫', 'XXL', '38', '10', '12');
-INSERT INTO `shopcart` VALUES ('5', 't-shirt005.jpg', '童装卫衣圆领熊本熊S6黑色衬衫', '中号', '138', '6', '1');
-INSERT INTO `shopcart` VALUES ('4', 't-shirt004.jpg', 'T恤山鸟叔鸟类学1白色衬衫', '中号', '38', '6', '23');
-INSERT INTO `shopcart` VALUES ('4', 't-shirt004.jpg', 'T恤山鸟叔鸟类学1白色衬衫', 'XL', '38', '9', '1');
-INSERT INTO `shopcart` VALUES ('4', 't-shirt004.jpg', 'T恤山鸟叔鸟类学1白色衬衫', '大号', '38', '4', '1');
-INSERT INTO `shopcart` VALUES ('47', 'shirt003.jpg', '凡客衬衫吉国武免烫领尖扣4.2深蓝维系格衬衫', '小号', '715', '3', '1');
-INSERT INTO `shopcart` VALUES ('4', 't-shirt004.jpg', 'T恤山鸟叔鸟类学1白色衬衫', '小号', '38', '6', '23');
-INSERT INTO `shopcart` VALUES ('4', 't-shirt004.jpg', 'T恤山鸟叔鸟类学1白色衬衫', 'XL', '38', '2', '23');
-INSERT INTO `shopcart` VALUES ('5', 't-shirt005.jpg', '童装卫衣圆领熊本熊S6黑色衬衫', '中号', '138', '7', '23');
-INSERT INTO `shopcart` VALUES ('5', 't-shirt005.jpg', '童装卫衣圆领熊本熊S6黑色衬衫', '小号', '138', '2', '23');
-INSERT INTO `shopcart` VALUES ('5', 't-shirt005.jpg', '童装卫衣圆领熊本熊S6黑色衬衫', '大号', '138', '2', '23');
-INSERT INTO `shopcart` VALUES ('4', 't-shirt004.jpg', 'T恤山鸟叔鸟类学1白色衬衫', '大号', '38', '2', '23');
-INSERT INTO `shopcart` VALUES ('47', 'shirt003.jpg', '凡客衬衫吉国武免烫领尖扣4.2深蓝维系格衬衫', '中号', '715', '2', '1');
-INSERT INTO `shopcart` VALUES ('4', 't-shirt004.jpg', 'T恤 山鸟叔 鸟类学1 白色衬衫', 'S', '38', '2', '1');
-INSERT INTO `shopcart` VALUES ('4', 't-shirt004.jpg', 'T恤 山鸟叔 鸟类学1 白色衬衫', 'L', '38', '1', '1');
-INSERT INTO `shopcart` VALUES ('47', 'shirt003.jpg', '凡客衬衫吉国武免烫领尖扣4.2深蓝维系格衬衫', '大号', '715', '1', '1');
 SET FOREIGN_KEY_CHECKS=1;
